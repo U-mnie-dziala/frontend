ec21f125ebc36b8abf4a458f367dff7fa3c4c07a
Mateusz Murawski <murawski.m@outlook.com>
2021-02-09 18:04:47 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
